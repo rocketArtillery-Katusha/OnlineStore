dc8e2ef2922134b0e37cbc4b268b3687f65c1f25
rocketArtillery-Katusha <tikt2664@gmail.com>
2023-09-22 21:08:11 +0300

    add file with routes

---
+++ client/src/common/routes.ts
@@ -0,0 +1,12 @@
+const routes = {
+    mainPage: '/',
+    addProductPage: '/add-product',
+    poductPage: '/pdouct:id',
+    adminPage: '/admin-page',
+    shoppingCartPage: '/shopping-cart',
+    signUp: '/signup',
+    signIn: 'signIn',
+    notFound: '*',
+};
+
+export default routes;
